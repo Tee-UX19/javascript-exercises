51c379635bb4c31b2f777bb85337f2afc3002509
TJ <king7tee.godwin@gmail.com>
2024-09-06 12:31:25 +0100

    completed exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(word,times) {
+    let result = '';
+
+    if (times < 0) { return 'ERROR'; }
+    else if(word === '') { return ''; }
+
+    for (let i = 0; i < times; i++) {
+        result += word;
+        console.log(word);
+    }
+    return result;
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
