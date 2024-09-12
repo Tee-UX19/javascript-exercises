bbb2305205f60ef22846fa4d509c4220843d6a4a
TJ <king7tee.godwin@gmail.com>
2024-09-12 14:19:00 +0100

    Completed tasks 3 and 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(String) {
+    let reversedString = "";
+    if (String === "") {
+        return "";
+    }
+    for (let i = String.length - 1; i >= 0; i--) {
+        reversedString += String[i];
+    }
+    return reversedString;
 
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...remove) {
+    for (let i = 0; i < remove.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (remove[i] === array[j]) {
+                array.splice(j, 1);
+                j--;
+                
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
