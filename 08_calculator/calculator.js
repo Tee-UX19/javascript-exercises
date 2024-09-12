33dcf528b9a2f04d413c39fae8d3bc3a329c0df3
TJ <king7tee.godwin@gmail.com>
2024-09-12 14:58:56 +0100

    completed task 7 & 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,48 @@
-const add = function() {
+const add = function(num1,num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1,num2) {
+	return num1 - num2;
+};
+
+const sum = function(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+};
+
+const multiply = function(array) {
+  let product;
+  if (array.length === 0) {
+    return 0;
+  }
+
+  product = array[0];
+  for (let i = 1; i < array.length; i++) {
+    product *= array[i];
+  }
+  return product;
+};
+
+
+const power = function(num1,num2) {
+  return Math.pow(num1,num2);
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = num; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 	
 };
 
